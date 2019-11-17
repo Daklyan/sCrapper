1575eda72bf51cc3fe8cb1ef7125127f281af728
Louis TARDY <36692889+Daklyan@users.noreply.github.com>
2019-11-17 18:13:33 +0100

    Update main.h

--- main.h
+++ main.h
@@ -15,11 +15,13 @@ typedef struct task{
     short sec;
     short min;
     short hour;
-    action* actionArray;
+    /*char** actionsName;*/action* actionArray;
+    int sizeActionArray;
 }task;
 
 void skipComment(FILE* file);
 long sizeOfFile(FILE* file);
 void initActionArray(struct action* actionArray, int sizeArray, FILE* file);
-void initTaskArray(struct task* taskArray, int sizeArray, FILE* file);
+void initTaskArray(task* taskArray, int nbTask, action* actionArray, int nbAction, FILE* file);
 int countOccurrences(FILE* file, char* word);
+void storeActions(task* taskArray, int index, action* actionArray, int sizeAction, FILE* file);
